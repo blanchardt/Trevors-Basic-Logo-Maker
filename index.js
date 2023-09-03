9af98034811f41b5656e19c1f8852191dded5cf8
unknown <blancardt@merrimack.edu>
2023-09-02 21:31:53 -0400

    adjusted text placements and added the writeFile in the index.js file.

--- index.js
+++ index.js
@@ -17,8 +17,14 @@ function init() {
         message: "Enter text for the logo.  (Must not be more than 3 characters.)",
         name: 'text',
         validate: function(text) {
-            if (text.length <= 3) {
+            //went to https://stackoverflow.com/questions/16048148/regex-to-check-if-whitespace-present to figure out how to check for spaces
+            //in regualr expressions.
+            var space = /\s/;
+            if (text.length <= 3 && !space.test(text)) {
               return true;
+            }
+            else if (space.test(text)) {
+                return "Must not have any spaces."
             }
             else {
               return "Must enter 3 or less characters.";
@@ -43,21 +49,32 @@ function init() {
       },
     ])
     .then((data) => {
-        console.log(data);
+        //create the shape of the logo.
         var completeShape;
+        var textLocation;
         if(data.shape === "circle"){
             completeShape = new Circle(data.shapeColor);
+            textLocation = '<text x="60" y="75" font-size="40"';
         }
         else if (data.shape === "square") {
             completeShape = new Square(data.shapeColor);
+            textLocation = '<text x="60" y="70" font-size="40"';
         }
         else {
             completeShape = new Triangle(data.shapeColor);
+            textLocation = '<text x="60" y="90" font-size="40"';
         }
+
+        //create the logo.
         var logo = new Logo(data.text, data.textColor, completeShape);
-        console.log(logo.shape.createShape());
-        console.log(logo.text);
-        console.log(logo.color);
+
+        //get the markup for the svg file.
+        var fileText = logo.createFileText(textLocation);
+        
+        //create a file if not already created and add the svg markup to it.
+        fs.writeFile(`./examples/${logo.text.toLowerCase()}Logo.svg`, fileText, (err) =>
+            test = err ? console.error(err) : console.log(`Generated ${logo.text.toLowerCase()}Logo.svg file!`)
+        );
     });
   }
   
